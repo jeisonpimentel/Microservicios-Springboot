1b17131d4f8942d4f3251fe41b7f000d916ccc24
jeisonpimentel <jeisonalvis10@gmail.com>
2022-02-21 18:01:19 -0300

    Se crea nuevo microservicios common

---
+++ udy-commons-examenes/src/test/java/com/udy/commons/examenes/UdyCommonsExamenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udy.commons.examenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdyCommonsExamenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
